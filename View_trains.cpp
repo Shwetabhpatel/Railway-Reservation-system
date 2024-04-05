b8ae48cad3cede0dae711173c8186b6d34cc4fb4
Akshara Gurav <87803953+Akshara-Gurav@users.noreply.github.com>
2024-04-05 14:11:30 +0530

    Delete View_trains.cpp

--- View_trains.cpp
+++
@@ -1,9 +0,0 @@
-void View_trains() {            // this function is used to show information about train fetched from  the text file.
-    ifstream file;
-    file.open("Train_schedule.txt");
-    string matter;
-    while (getline(file, matter)) {
-        cout << matter << endl;
-    }
-    file.close();
-}
